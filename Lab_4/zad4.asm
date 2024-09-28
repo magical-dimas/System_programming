fc4a5ff8505faf41426ee8e102d5f3cd1b2a8035
magical-dimas <85245869+magical-dimas@users.noreply.github.com>
2024-09-28 12:03:27 +0000

    lab4 zad4

---
+++ Lab_4/zad4.asm
@@ -0,0 +1,91 @@
+format ELF64
+
+public _start
+
+include '/workspaces/System_programming/Lab_3/func.asm'
+
+section '.bss' writable
+input dq ?
+output dq ?
+
+section '.text' executable
+_start:
+mov rax, 0
+mov rdi, 0
+mov rsi, input
+mov rdx, 255
+syscall
+mov rax, input
+call len_str
+mov byte [input + rax], 0
+call str_number
+mov rsi, rax
+xor r8, r8
+
+.l1:
+mov rax, rsi
+add rax, 8
+mov rbx, rsi
+add rbx, 4
+mul rbx
+mul rsi
+mov rbx, rax
+mov rax, rsi
+mov rcx, 2
+div rcx
+cmp rdx, 0
+jne .negat
+add r8, rbx
+jmp .cont
+.negat:
+sub r8, rbx
+.cont:
+sub rsi, 1
+cmp rsi, 0
+jg .l1
+
+mov rax, r8
+call print_num
+mov rax, 0xA
+call print
+
+call exit
+
+print:
+push rcx
+mov [output], rax
+mov eax, 1
+mov edi, 1
+mov rsi, output
+mov edx, 1
+syscall
+pop rcx
+ret
+
+print_num:
+xor rbx, rbx
+mov rcx, 10
+test rax, rax
+jns .loop
+push rax
+mov rax, '-'
+call print
+pop rax
+neg rax
+
+.loop:
+xor rdx, rdx
+div rcx
+push rdx
+inc rbx
+cmp rax, 0
+jne .loop
+
+.print_loop:
+pop rax
+add rax, 48
+call print
+dec rbx
+cmp rbx, 0
+jne .print_loop
+ret
