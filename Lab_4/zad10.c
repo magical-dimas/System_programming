9ec96c0dd62abe2126d4973983cf188885ee72d3
magical-dimas <85245869+magical-dimas@users.noreply.github.com>
2024-09-30 17:48:38 +0000

    c part done lab4

--- Lab_4/zad10.c
+++ Lab_4/zad10.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-    
+    char password [] = "qwerty321";
+    char in [20];
+    for(int i = 0; i < 5; i++){
+        scanf("%s", in);
+        if(strcmp(in, password) == 0){
+            printf("Вошли\n");
+            return 0;
+        } //!!!
+        printf("Невереный пароль\n");
+    }
+    printf("Неудача\n");
     return 0;
 }
